fb82fdf45457b764d344f2dcb72585d3679d4e63
jongracias <Jon.gracias@gmail.com>
2021-03-21 22:33:02 -0400

    project

---
+++ HTML5CH2/wordGame/tellStory.js
@@ -0,0 +1,56 @@
+function tellStory() {
+    //gather form elements
+    var txtCharacter = document.getElementById("txtCharacter");
+    var txtWeapon = document.getElementById("txtWeapon");
+    var txtSkill = document.getElementById("txtSkill");
+    var txtSkill2 = document.getElementById("txtSkill2");
+    var txtShip = document.getElementById("txtShip");
+    var txtCompanion = document.getElementById("txtCompanion");
+    var txtRelationship = document.getElementById("txtRelationship")
+    var txtPlanet = document.getElementById("txtPlanet")
+    var output = document.getElementById("output")
+
+    //create variables for input
+    var character = txtCharacter.value;
+    var weapon = txtWeapon.value;
+    var skill = txtSkill.value;
+    var skill2 = txtSkill2.value;
+    var ship = txtShip.value;
+    var companion = txtCompanion.value;
+    var relationship = txtRelationship.value;
+    var planet = txtPlanet.value;
+
+    if (character == "Cartwright") {
+        if (companion == "Yuri") {
+            if (planet == "Klendathu") {
+                var story = character + " was with " + companion;
+                story += ". They were landing " + ship + " on the planet " + planet + ".";
+                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
+                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
+                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
+                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
+                story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
+                story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
+                story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
+                output.innerHTML = story
+            } else {
+                var story = character + " was with " + companion;
+                story += ". They were landing " + ship + " on the planet " + planet + ".";
+                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
+                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
+                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
+                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
+                story += companion + " Then used his sacred power of " + skill2 + " killing Zorak.";
+                story += "Sadly " + companion + "turned around and realized during the fight " + character + " was mortally wounded."
+                story += companion + " searched the planet for the treasure of " + relationship + ". Unfourtunatley he never."
+                output.innerHTML = story
+            }
+        } else {
+            var story = "";
+        }
+    } else {
+        var story = "";
+    }
+}
+
+
