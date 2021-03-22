5abc69c8018a2cdfd6fc0f25bf7b0d8a0185e3f8
Jon Gracias <jonpgracias@gmail.com>
2021-03-21 22:50:41 -0400

    Added comments to explain story

--- HTML5CH2/wordGame/tellStory.js
+++ HTML5CH2/wordGame/tellStory.js
@@ -22,18 +22,35 @@ function tellStory() {
 
     if (character == "Cartwright") {
         if (companion == "Yuri") {
-            if (planet == "Klendathu") {
-                var story = character + " was with " + companion;
-                story += ". They were landing " + ship + " on the planet " + planet + ".";
-                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
-                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
-                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
-                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
-                story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
-                story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
-                story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
-                output.innerHTML = story
+            if (planet == "Klendathu") { 
+                if(relationship == "Time Machine") {
+                    //Everything is right 
+                    var story = character + " was with " + companion;
+                    story += ". They were landing " + ship + " on the planet " + planet + ".";
+                    story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
+                    story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
+                    story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
+                    story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
+                    story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
+                    story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
+                    story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
+                    output.innerHTML = story
+
+                } else {
+                    //Everything is right but treasure is not a time machine
+                    var story = character + " was with " + companion;
+                    story += ". They were landing " + ship + " on the planet " + planet + ".";
+                    story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
+                    story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
+                    story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
+                    story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
+                    story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
+                    story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
+                    story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
+                    output.innerHTML = story
+                }
             } else {
+                //Cartwright and Yuri travel together but to the wrong planet
                 var story = character + " was with " + companion;
                 story += ". They were landing " + ship + " on the planet " + planet + ".";
                 story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
@@ -46,9 +63,20 @@ function tellStory() {
                 output.innerHTML = story
             }
         } else {
-            var story = "";
+            //Cartwright does not travel with Yuri planet is not important
+            var story = character + " was with " + companion;
+                story += ". They were landing " + ship + " on the planet " + planet + ".";
+                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
+                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
+                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
+                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
+                story += companion + " Then used his sacred power of " + skill2 + " killing Zorak.";
+                story += "Sadly " + companion + "turned around and realized during the fight " + character + " was mortally wounded."
+                story += companion + " searched the planet for the treasure of " + relationship + ". Unfourtunatley he never."
+                output.innerHTML = story
         }
     } else {
+        //Story is about meeting Cartwright and Yuri that tell you of great treasure 
         var story = "";
     }
 }
