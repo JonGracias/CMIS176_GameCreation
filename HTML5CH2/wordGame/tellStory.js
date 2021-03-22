d269960aa6fd0002ef229832c8aa34e33c6e1350
jongracias <Jon.gracias@gmail.com>
2021-03-22 17:22:51 -0400

    Final, project 1

--- HTML5CH2/wordGame/tellStory.js
+++ HTML5CH2/wordGame/tellStory.js
@@ -1,84 +1,67 @@
+/*Jonathan Gracias
+  tellStory.js
+  03/22/2021
+  This file has three functions. Function 1 takes the value from input elements. 
+  It then creates variables for the input.
+  Then it writes the story into a 'story' variable.
+  Function 2 uses a parameter that gets the story from function one. The function then prints the
+  story to the screen in a typing animation. 
+  Function 3 simply reloads the page when "Reset" button is clicked*/
+
+
+
 function tellStory() {
     //gather form elements
     var txtCharacter = document.getElementById("txtCharacter");
     var txtWeapon = document.getElementById("txtWeapon");
-    var txtSkill = document.getElementById("txtSkill");
-    var txtSkill2 = document.getElementById("txtSkill2");
+    var txtAdjective = document.getElementById("txtAdjective");
+    var txtPronoun = document.getElementById("txtPronoun");
     var txtShip = document.getElementById("txtShip");
     var txtCompanion = document.getElementById("txtCompanion");
-    var txtRelationship = document.getElementById("txtRelationship")
-    var txtPlanet = document.getElementById("txtPlanet")
-    var output = document.getElementById("output")
+    var txtNoun = document.getElementById("txtNoun");
+    var txtPlanet = document.getElementById("txtPlanet");
+
 
     //create variables for input
     var character = txtCharacter.value;
-    var weapon = txtWeapon.value;
-    var skill = txtSkill.value;
-    var skill2 = txtSkill2.value;
-    var ship = txtShip.value;
     var companion = txtCompanion.value;
-    var relationship = txtRelationship.value;
+    var adjective = txtAdjective.value;
+    var pronoun = txtPronoun.value;
+    var ship = txtShip.value;
     var planet = txtPlanet.value;
+    var noun = txtNoun.value;
+    var weapon = txtWeapon.value;
 
-    if (character == "Cartwright") {
-        if (companion == "Yuri") {
-            if (planet == "Klendathu") { 
-                if(relationship == "Time Machine") {
-                    //Everything is right 
-                    var story = character + " was with " + companion;
-                    story += ". They were landing " + ship + " on the planet " + planet + ".";
-                    story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
-                    story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
-                    story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
-                    story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
-                    story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
-                    story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
-                    story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
-                    output.innerHTML = story
 
-                } else {
-                    //Everything is right but treasure is not a time machine
-                    var story = character + " was with " + companion;
-                    story += ". They were landing " + ship + " on the planet " + planet + ".";
-                    story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
-                    story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
-                    story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
-                    story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
-                    story += character + " Then used his sacred power of " + skill2 + " killing Zorak.";
-                    story += "Sadly " + character + "turned around and realized during the fight " + companion + " was mortally wounded."
-                    story += character + " was devestated. Luckily, the treasure of " + relationship + " was a time machine."
-                    output.innerHTML = story
-                }
-            } else {
-                //Cartwright and Yuri travel together but to the wrong planet
-                var story = character + " was with " + companion;
-                story += ". They were landing " + ship + " on the planet " + planet + ".";
-                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
-                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
-                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
-                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
-                story += companion + " Then used his sacred power of " + skill2 + " killing Zorak.";
-                story += "Sadly " + companion + "turned around and realized during the fight " + character + " was mortally wounded."
-                story += companion + " searched the planet for the treasure of " + relationship + ". Unfourtunatley he never."
-                output.innerHTML = story
-            }
-        } else {
-            //Cartwright does not travel with Yuri planet is not important
-            var story = character + " was with " + companion;
-                story += ". They were landing " + ship + " on the planet " + planet + ".";
-                story += "Their intel told them the acient treasure of " + relationship + "was on this planet" 
-                story += "as soon as they got of the ship Zorak jumped from behind the landing gears"
-                story += " but " + character + " was ready to attack with " + weapon + " He blasted a shot but missed!";
-                story += " Zorak used his insect skill of " + skill + " leaving " + character + " confused.";
-                story += companion + " Then used his sacred power of " + skill2 + " killing Zorak.";
-                story += "Sadly " + companion + "turned around and realized during the fight " + character + " was mortally wounded."
-                story += companion + " searched the planet for the treasure of " + relationship + ". Unfourtunatley he never."
-                output.innerHTML = story
-        }
-    } else {
-        //Story is about meeting Cartwright and Yuri that tell you of great treasure 
-        var story = "";
+
+    //write story  
+    var story = character + " looked in horror as " + companion + "'s ship was ";
+    story += "blown into " + adjective + " pieces by the evil " + pronoun;
+    story += " Zorak. Luckily, " + companion + " was able to escape on the escape ship named " + ship;
+    story += ". " + companion + " was headed to the nearby planet " + planet + " that was known for it's " + noun;
+    story += ". " + character + " knew that the only weapon that could defeat Zorak was " + weapon + ".";
+
+    //Pass story to typeWriter().
+    typeWriter(story);
+}
+
+//Creates i and speed variable for typeWriter()
+var i = 0;
+var speed = 100;
+
+//Takes story and types it on screen one letter at a time
+function typeWriter(story) {
+    var txt = story;
+
+    if (i < txt.length) {
+        document.getElementById("output").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(tellStory, speed);
     }
 }
 
+//Reloads page onclick "Reset" button.
+function formReset() {
+    location = location;
+}
 
